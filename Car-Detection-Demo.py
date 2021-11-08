713bc877cf261bde943b0cdf467b441aca4857b2
RAljowair <rayanaljowair@gmail.com>
2021-11-09 02:52:49 +0300

    edit for video read

--- Car-Detection-Demo.py
+++ Car-Detection-Demo.py
@@ -4,14 +4,13 @@ import numpy as np
 import tensorflow as tf
 
 vid = cv.VideoCapture('video/Vehicles ongoing traffic.mp4')
-if(vid.isOpened() == False):
+if (vid.isOpened() == False):
     print("ERROR opening the video file")
 hight = 1080
 width = 720
 car_cascade = cv.CascadeClassifier('cars.xml')
-cars_number = 0
 
-while(vid.isOpened()):
+while (vid.isOpened()):
     ret, frame = vid.read()
     if ret == True:
         # '1' property ID of CV_CAP_PROP_POS_FRAME (takes current frame number).
@@ -23,8 +22,7 @@ while(vid.isOpened()):
             cars = car_cascade.detectMultiScale(frame, 1.1, 1)
             # Draw border around the car
             for (x, y, w, h) in cars:
-                cv.rectangle(frame, (x,y), (x+w,y+h), (0,0,256), 2)
-                cars_number += 1
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 256), 2)
             cv.imshow('Demo Test', frame)
             key = cv.waitKey(30)
             if key == ord('q'):
