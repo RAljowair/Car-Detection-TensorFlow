34fddaaedc3ac4f03bb994b111c1268a5e98c83f
RAljowair <rayanaljowair@gmail.com>
2021-11-09 00:31:54 +0300

    first commit

---
+++ Car-Detection-Demo.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+vid = cv.VideoCapture('video/Vehicles ongoing traffic.mp4')
+if(vid.isOpened() == False):
+    print("ERROR opening the video file")
+hight = 1080
+width = 720
+car_cascade = cv.CascadeClassifier('cars.xml')
+cars_number = 0
+
+while(vid.isOpened()):
+    ret, frame = vid.read()
+    if ret == True:
+        # '1' property ID of CV_CAP_PROP_POS_FRAME (takes current frame number).
+        if vid.get(1) % 5 == 0:
+            # convert video to grayscale.
+            rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+            cars = car_cascade.detectMultiScale(frame, 1.1, 1)
+            # Draw border around the car
+            for (x, y, w, h) in cars:
+                cv.rectangle(frame, (x,y), (x+w,y+h), (0,0,256), 2)
+                cars_number += 1
+            cv.imshow('Demo Test', frame)
+            key = cv.waitKey(30)
+            if key == ord('q'):
+                break
+    else:
+        break
+
+vid.release()
+cv.destroyAllWindows()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# model_name = "mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28"
+# masking_model = load_model(model_name)
+# masking_model.output_shapes
+# for image_path in TEST_IMAGE_PATHS:
+#   show_inference(masking_model, image_path)
